8242e32222a5055333204f2bcdec2cbbc8d71be3
Helmi <121215988+helmisaidan@users.noreply.github.com>
2025-10-23 00:02:34 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-const menuIcon = document.getElementById("menuIcon");
-const navMenu = document.getElementById("navMenu");
 
-menuIcon.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-
-  // Tukar ikon bila menu buka/tutup
-  if (navMenu.classList.contains("show")) {
-    menuIcon.innerHTML = "&times;";
-  } else {
-    menuIcon.innerHTML = "&#9776;";
-  }
-});
