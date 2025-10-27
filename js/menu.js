325946a8451e5f09c657d166cceae2c909d91bf5
Helmi <121215988+helmisaidan@users.noreply.github.com>
2025-10-27 17:48:49 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+<script>
 function myFunction() {
   const x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -6,3 +7,4 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+</script>
