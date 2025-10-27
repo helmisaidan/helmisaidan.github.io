c725ff6969400f458527f796412d221636e61ccc
Helmi <121215988+helmisaidan@users.noreply.github.com>
2025-10-27 13:52:08 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,6 @@
-// Neon hamburger toggle
-const toggleBtn = document.getElementById("menu-toggle");
-const nav = document.getElementById("main-nav");
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('header nav');
 
-toggleBtn.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  toggleBtn.classList.toggle("active");
-});
-
-// If screen resized up to desktop, always show nav & reset icon
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 900) {
-    nav.classList.remove("open");
-    toggleBtn.classList.remove("active");
-  }
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('show');
 });
