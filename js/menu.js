5af679d9b04bb8e252e1e422a25e3c575cef6e51
Helmi <121215988+helmisaidan@users.noreply.github.com>
2025-11-11 18:05:16 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navMenu = document.getElementById("nav-menu");
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
 
-  menuToggle.addEventListener("click", () => {
-    const isOpen = navMenu.classList.toggle("show");
-    menuToggle.classList.toggle("active", isOpen);
-
-    // Disable background scrolling when menu is open
-    document.body.style.overflow = isOpen ? "hidden" : "auto";
-  });
+menuToggle.addEventListener("click", function() {
+  this.classList.toggle("change"); // animasi X
+  navMenu.classList.toggle("show"); // buka/tutup menu
 });
