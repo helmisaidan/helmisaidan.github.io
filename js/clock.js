f1a97fe66e123ec1b091c81714c41360952c3924
Helmi <121215988+helmisaidan@users.noreply.github.com>
2025-11-14 21:12:53 +0800

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+// ========== Clock ==========
+
+function updateClock() {
+
+  const clock = document.getElementById("clock");
+
+  const now = new Date();
+
+  clock.textContent = now.toLocaleTimeString();
+
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
